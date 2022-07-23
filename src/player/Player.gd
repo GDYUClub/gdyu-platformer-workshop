979eaed7d64f7f97c10a1eb58d1b424e9377f3b2
JUB_Yoush <yoush1337@gmail.com>
2022-07-22 21:26:18 -0400

    updated

--- src/player/Player.gd
+++ src/player/Player.gd
@@ -1,10 +1,10 @@
 extends KinematicBody2D
 
-export (int) var speed = 1200
-export (int) var jump_speed = -1800
-export (int) var gravity = 4000
+export (int) var speed = 700
+export (int) var jump_speed = -800
+export (int) var gravity = 2000
 
-export (float, 0, 1.0) var friction = 0.11
+export (float, 0, 1.0) var friction = 0.2
 export (float, 0, 1.0) var acceleration = 0.3
 
 var dir = Vector2.ZERO
@@ -17,15 +17,11 @@ func get_input():
 
 func _physics_process(delta):
 	get_input()
-	velocity.x += speed * dir.x
 	velocity.y += gravity * delta
-	
 	if dir.x != 0:
 		velocity.x = lerp(velocity.x, dir.x * speed, acceleration)
 	else:
 		velocity.x = lerp(velocity.x, 0, friction)
 	velocity = move_and_slide(velocity, Vector2.UP)
-	
-	if Input.is_action_just_pressed("jump"):
-		if is_on_floor():
-			velocity.y = jump_speed
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = jump_speed
